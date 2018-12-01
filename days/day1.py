07eb54ec263aab7de1f86bb173fca767c4725598
Kevin Alberts <kevin@kevinalberts.nl>
2018-12-02 00:50:22 +0100

    Optimize day 1

--- days/day1.py
+++ days/day1.py
@@ -3,24 +3,19 @@ from days import AOCDay, day
 
 @day(1)
 class DayOne(AOCDay):
-    frequency = 0
+    def part1(self, input_data):
+        yield sum(i for i in map(int, input_data))
 
-    def common(self):
-        self.frequency = 0
-
-    def part1(self):
-        for i in self.input_data:
-            self.frequency += i
-        yield str(self.frequency)
-
-    def part2(self):
+    def part2(self, input_data):
+        frequency = 0
         unique_frequencies = set()
+        data = list(map(int, input_data))
 
         while True:
-            for i in self.input_data:
-                if self.frequency in unique_frequencies:
-                    yield "DUP FREQ: {}".format(self.frequency)
+            for i in data:
+                if frequency in unique_frequencies:
+                    yield frequency
                     return
-                else:
-                    unique_frequencies.add(self.frequency)
-                self.frequency += i
+
+                unique_frequencies.add(frequency)
+                frequency += i
